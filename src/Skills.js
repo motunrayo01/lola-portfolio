bbafe05cf0c16c7249d55f2993469fac0846a5a1
motunrayo01 <53899116+motunrayo01@users.noreply.github.com>
2020-08-24 07:09:53 -0300

    update

--- src/Skills.js
+++ src/Skills.js
@@ -6,20 +6,21 @@ class Skills extends Component{
     return (
       <div> 
          
-           <h2> SKILLS </h2>
-              
+         <h2> SKILLS </h2>
+           
              
-                <p> Front End </p>
+                <div className='frontend'>
+                
                 <ul>
                 <li> HTML  <ProgressBar variant="success" animated now={97}/> </li> <br/>
                   <li> CSS  <ProgressBar variant="success" animated now={97}/> </li> <br/>
                   <li> Css framework (bootstrap) </li> <ProgressBar variant="success" animated now={97}/> <br/>
-                  <li> Css Javascript </li> <ProgressBar variant="success" animated now={75}/> <br/>
-                  <li> Javascript framework (React)</li>  <ProgressBar variant="success"  animated now={50}/><br/>
+                  <li> Javascript </li> <ProgressBar variant="success" animated now={75}/> <br/>
+                  <li> Javascript framework (React)</li>  <ProgressBar variant="success"  animated now={60}/><br/>
                  
                   
                   </ul> 
-                
+                  </div>                
                 
             
       </div>
