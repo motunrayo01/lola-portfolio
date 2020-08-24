bb17da199e23e24bf58ebb4443486de412252bc8
motunrayo01 <53899116+motunrayo01@users.noreply.github.com>
2020-08-23 21:06:36 -0300

    update

--- src/Projects.js
+++ src/Projects.js
@@ -13,37 +13,38 @@ import finances from './finances.PNG';
                     <h2>SIMPLE CALCULATOR </h2>
                      <p>This was designed using HTML,CSS and Javascript. 
                         The calculator can solve simple scientific problems </p>
-                     <img src={project1} alt='profile' width="50%" />
+                     <img src={project1} alt='profile' width="40%" />  <br/> <br/>
                      < a href= 'https://motunrayo01.github.io/calculator' className="badge badge-pill 
-                    badge-primary"> calculator</a>
+                    badge-primary"> calculator</a>  <br/> <br/>
                     </div>
                     <div>
                      <h2>COUNTER </h2>
                      <p>A counter in which number increases when the ADD COUNT 
                         button is clicked on and number decreases when the LOWER COUNT is clicked on </p>
-                     <img src={project2} alt='profile' width="50%" />
+                     <img src={project2} alt='profile' width="40%" />  <br/>  <br/>
                     < a href= 'https://motunrayo01.github.io/counter' className="badge badge-pill 
-                    badge-primary"> counter</a>
+                    badge-primary"> counter</a>  <br/> <br/>
                     </div>
                     <div>
-                     <h2>shopping catalague</h2>
+                     <h2>CATALOGUE WEB-PAGE</h2>
                      <p> A shopping website for all types of colognes deigned with HTML and CSS. 
                          A youtube video is also embedde in the site. the sites has various link to 
                          different types of colognes
                       </p>
-                     <img src={project3} alt='profile' width="50%" />
+                     <img src={project3} alt='profile' width="50%" />  <br/>  <br/>
                   < a href= 'https://github.com/motunrayo01/shopping-catalague'
                        className="badge badge-pill 
-                    badge-primary"> webpage</a> 
+                    badge-primary"> webpage</a>  <br/> <br/>
                     </div>
                     <div>
-                    <h2>SIMPLE CALCULATOR </h2>
+                    <h2>FINANCES WEB-PAGE </h2>
                      <p> A web page designed with react and bootstrap
                       </p>
-                      <img src={finances} alt='profile' width="30%" />
+                      <img src={finances} alt='profile' width="50%" /> <br/>  <br/>
+
                     
                         < a href= 'Finances-page.netlify.app' className="badge badge-pill 
-                    badge-primary"> Finances Webpage</a>
+                    badge-primary"> Finances Webpage</a>  <br/> <br/>
                      </div>
 
                     < a href= 'https://github.com/motunrayo01' className="badge badge-pill 
